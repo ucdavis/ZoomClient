d4a49168f3af1ed3699302ddab308fc3d7f19f3f
Edward Henn <emhenn@ucdavis.edu>
2020-12-09 09:06:15 -0800

    add logging; inject zoom client and options to home controller; run test in home index

--- samples/Sample.Web/Controllers/HomeController.cs
+++ samples/Sample.Web/Controllers/HomeController.cs
@@ -5,22 +5,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Sample.Web.Models;
+using ZoomClient;
 
 namespace Sample.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly Zoom _zoomClient;
+        private readonly ZoomClient.Options _options;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ZoomClient.Zoom zoomClient, IOptions<ZoomClient.Options> options)
         {
             _logger = logger;
+            _zoomClient = zoomClient;
+            _options = options.Value;
+
+            _zoomClient.Options = _options;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var user = _zoomClient.GetUser("me");
+
+            return View(user);
         }
 
         public IActionResult Privacy()
